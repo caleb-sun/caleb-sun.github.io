d9323c8ab71e6d73e45c32aa9943767d8b80b3eb
capreesun <134004837+caleb-sun@users.noreply.github.com>
2023-10-28 09:44:25 -0400

    major update

---
+++ javascript/tlj.js
@@ -0,0 +1,64 @@
+
+tlj = document.getElementById("tlj");
+context = document.getElementById("tlj-context-marker");
+problem = document.getElementById("tlj-problem-marker");
+research = document.getElementById("tlj-research-marker");
+contextual = document.getElementById("tlj-contextual-marker");
+concept = document.getElementById("tlj-concept-marker");
+visual = document.getElementById("tlj-visual-marker");
+demo = document.getElementById("tlj-demo-marker");
+
+
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 300) {
+        tlj.style.display = "flex";
+    } else {
+        tlj.style.display = "none";
+    }
+    if (y<=3249) {
+        context.style.textDecoration="none";
+    }
+    if (y>=3250) {
+        context.style.textDecoration = "underline";
+        context.style.textDecorationStyle = "dotted";
+        problem.style.textDecoration = "none";
+    }
+    if (y>=4470){
+        context.style.textDecoration = "none";
+        problem.style.textDecoration = "underline";
+        problem.style.textDecorationStyle = "dotted";
+        research.style.textDecoration = "none";
+    }
+    if (y>=5470){
+        problem.style.textDecoration = "none";
+        research.style.textDecoration = "underline";
+        research.style.textDecorationStyle = "dotted";
+        contextual.style.textDecoration="none";
+    }
+    if (y>=7100){
+        research.style.textDecoration = "none";
+        contextual.style.textDecoration = "underline";
+        contextual.style.textDecorationStyle = "dotted";
+        concept.style.textDecoration="none";
+    }
+    if (y>=8880){
+        contextual.style.textDecoration = "none";
+        concept.style.textDecoration = "underline";
+        concept.style.textDecorationStyle = "dotted";
+        visual.style.textDecoration="none";
+    }
+    if (y>=12730){
+        concept.style.textDecoration = "none";
+        visual.style.textDecoration = "underline";
+        visual.style.textDecorationStyle = "dotted";
+        demo.style.textDecoration="none";
+    }
+    if (y>=13580){
+        visual.style.textDecoration = "none";
+        demo.style.textDecoration = "underline";
+        demo.style.textDecorationStyle = "dotted";
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
