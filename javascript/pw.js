cd29257fa35414a47e68078e9f4f5a31f91fd536
capreesun <134004837+caleb-sun@users.noreply.github.com>
2023-10-28 13:00:50 -0400

    update pw

---
+++ javascript/pw.js
@@ -0,0 +1,8 @@
+function protectPassword() {
+    a = document.getElementById('pass');
+    test = document.getElementById('a');
+    if (a.value === 'calebsun2023') {
+        window.open("https://calebsun.co/projects/csl.html", "_self");
+    } else {
+    }
+}
