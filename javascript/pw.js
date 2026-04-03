0600c93e13054ade8165d659154c2fdd659946d4
caleb-sun <sun.cs0327@gmail.com>
2026-04-02 23:16:05 -0400

    reno

--- javascript/pw.js
+++
@@ -1,8 +0,0 @@
-function protectPassword() {
-    a = document.getElementById('pass');
-    test = document.getElementById('a');
-    if (a.value === 'calebsun2023') {
-        window.open("https://calebsun.co/projects/csl.html", "_self");
-    } else {
-    }
-}
